b9807bd84111e94ceed1ed4a07cf36878d37a1d9
CSPablo <cspablo1540@gmail.com>
2024-01-07 13:04:05 +0100

    Arreglos 	modified:   bootsrap/js/js.js

--- bootsrap/js/js.js
+++ bootsrap/js/js.js
@@ -1,12 +1,11 @@
 window.onload = function(){
     var modalRegistro = new bootstrap.Modal(document.getElementById("modalRegistro"),{});
-    modalRegistro.show();
     document.getElementById("registro").onclick = function(){
         modalRegistro.show();
     }
-/*
+
     var modalCookies = new bootstrap.Modal(document.getElementById("modalCookies"),{});
     modalCookies.show();
-*/
+
 
 }
